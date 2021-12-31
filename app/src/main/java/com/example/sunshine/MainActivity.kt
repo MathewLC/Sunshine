2627c2092aa4dd0a7f50ba4990dc305c5cd948c4
MatthewLC <mateusluiztj@hotmail.com>
2021-12-31 13:56:53 -0300

    Lesson 01 Exercise 02

--- app/src/main/java/com/example/sunshine/MainActivity.kt
+++ app/src/main/java/com/example/sunshine/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.sunshine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  _weatherDisplayTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
+
+        _weatherDisplayTextView = findViewById(R.id.tv_weather_data)
+
+        WeatherMock.getDummyWeatherData().forEach { stringLine ->
+            _weatherDisplayTextView.append("$stringLine \n\n\n")
+        }
+
+
     }
 }
