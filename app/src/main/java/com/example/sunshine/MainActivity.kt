647958f1902a99f23c468523a7bbaa8dc29a72dd
MatthewLC <mateusluiztj@hotmail.com>
2022-01-08 19:28:42 -0300

    Lesson 02 Exercise 02

--- app/src/main/java/com/example/sunshine/MainActivity.kt
+++ app/src/main/java/com/example/sunshine/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.sunshine
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import com.example.sunshine.data.SunshinePreferences
 import com.example.sunshine.utilities.NetworkUtils
 import java.io.IOException
 import java.net.URL
+import com.example.sunshine.R.id.action_refresh
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +22,11 @@ class MainActivity : AppCompatActivity() {
 
         _weatherDisplayTextView = findViewById(R.id.tv_weather_data)
 
-        loadWeatherData()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.forecast,menu)
+        return true
     }
 
     inner class WeatherQueryTask : AsyncTask<URL, Void, String>() {
@@ -38,6 +45,15 @@ class MainActivity : AppCompatActivity() {
             _weatherDisplayTextView.text = result
         }
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == action_refresh) {
+            loadWeatherData()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun loadWeatherData() {
